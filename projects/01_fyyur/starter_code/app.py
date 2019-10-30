de82624c81293ffc42abebd4bc3e321d11159d7c
Derrick Sekidde <kidricederek@gmail.com>
2019-10-30 07:54:12 +0300

    Project is officially done.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -13,6 +13,7 @@ from logging import Formatter, FileHandler
 from flask_migrate import Migrate
 from flask_wtf import Form
 from forms import *
+from models import db, Artist, Show, Venue
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,7 +21,7 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
@@ -28,63 +29,6 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-  __tablename__ = 'Venue'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  genres = db.Column(db.ARRAY(db.String))
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  address = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  website = db.Column(db.String)
-  image_link = db.Column(db.String(300))
-  seeking_talent = db.Column(db.Boolean)
-  seeking_description = db.Column(db.String)
-  facebook_link = db.Column(db.String(120))
-  shows = db.relationship('Show', backref=db.backref('Venue',cascade="all,delete"))
-
-  def __repr__(self):
-    return f'<Venue:{self.name}>'
-
-  
-  
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-  __tablename__ = 'Artist'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  genres = db.Column(db.ARRAY(db.String))
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  website = db.Column(db.String)
-  image_link = db.Column(db.String(200))
-  facebook_link = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean)
-  seeking_description = db.Column(db.String)
-  shows = db.relationship('Show', backref=db.backref('Artist',cascade="all,delete"))
-
-  def __repr__(self):
-    return f'<Artist: {self.id}, {self.name}>'
-
-class Show(db.Model):
-  __tablename__ = 'Show'
-  
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-  def __repr__(self):
-    return f'<Show:art {self.id}>'
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
 # Filters.
