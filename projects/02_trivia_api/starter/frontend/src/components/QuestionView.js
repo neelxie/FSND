9b0d3cfca4d83a8103fdf04fb5659073336f9433
Derrick Sekidde <kidricederek@gmail.com>
2019-11-20 11:50:25 +0300

    Align frontend urls to backend.

--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -60,7 +60,7 @@ class QuestionView extends Component {
 
   getByCategory= (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `http://127.0.0.1:5000/categories/${id}/questions`,
       type: "GET",
       success: (result) => {
         this.setState({
@@ -105,7 +105,7 @@ class QuestionView extends Component {
     if(action === 'DELETE') {
       if(window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
-          url: `/questions/${id}`, //TODO: update request URL
+          url: `http://127.0.0.1:5000/questions/${id}`,
           type: "DELETE",
           success: (result) => {
             this.getQuestions();
